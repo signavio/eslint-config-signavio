536b33461d6cbb8d416b6b5732f1fb095cd4197e
David Bialik <david.bialik@signavio.com>
2017-06-02 14:57:26 +0200

    Rewrite eslint-config-signavio without changing a lot
    * Delete `index.js` (replaced with `.eslintrc.js`)
    * Delete `.eslintrc.json` (replaced with `.eslintrc.js`)
    * Bump the version (`package.json`)
    * Change the `main` attribute in the `package.json`
    * Add npm scripts for
     * Linting
     * Formatting
    * Add myself as contributor
    * Update and replace dependencies
    * Update `yarn.lock` file

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+    extends: ['airbnb', 'prettier', 'prettier/flowtype', 'prettier/react'],
+    env: {
+        browser: true,
+    },
+    plugins: ['prettier', 'flowtype'],
+    rules: {
+        'react/jsx-filename-extension': ['warn', { extensions: ['.js'] }],
+        'react/sort-comp': [
+            'warn',
+            {
+                order: [
+                    'static-methods',
+                    'lifecycle',
+                    'render',
+                    'everything-else',
+                ],
+            },
+        ],
+        'flowtype/define-flow-type': 'warn',
+        'flowtype/require-parameter-type': 'warn',
+        'flowtype/use-flow-type': 'warn',
+
+        // eslint-config-prettier rules that might cause problems
+        // see: https://github.com/prettier/eslint-config-prettier#special-rules
+        curly: ['error', 'multi-line'],
+        'max-len': [
+            'error',
+            80,
+            4,
+            {
+                ignoreUrls: true,
+                ignoreComments: false,
+                ignoreRegExpLiterals: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true,
+            },
+        ],
+        'no-confusing-arrow': [
+            'error',
+            {
+                allowParens: true,
+            },
+        ],
+        'no-mixed-operators': [
+            'off',
+            {
+                allowSamePrecedence: true,
+            },
+        ],
+        'no-tabs': 'error',
+        quotes: [
+            'off',
+            'single',
+            {
+                avoidEscape: true,
+            },
+        ],
+
+        // eslint-plugin-prettier rules
+        'prettier/prettier': [
+            'error',
+            {
+                tabWidth: 4,
+                semi: false,
+                singleQuote: true,
+                trailingComma: 'all',
+            },
+        ],
+    },
+    settings: {
+        flowtype: {
+            onlyFilesWithFlowAnnotation: true,
+        },
+    },
+}
