cb4530fd671cee981f2f0204c96c019e0852db01
Philipp Giese <giese.philipp+git@gmail.com>
2017-09-21 19:35:14 +0200

    changed prettier config in order, so that eslint can actually pick it up

--- prettier.config.js
+++ prettier.config.js
@@ -5,12 +5,10 @@ const prettierConfig = {
     trailingComma: 'es5',
 }
 
-module.exports = Object.assign({}, prettierConfig, {
-    overrides: [
-        {
-            files: 'package.json',
-            options: { tabWidth: 2, parser: 'json' },
-        },
-    ],
-    prettierConfig,
-})
+exports.prettierConfig = prettierConfig
+exports.overrides = [
+    {
+        files: 'package.json',
+        options: { tabWidth: 2, parser: 'json' },
+    },
+]
